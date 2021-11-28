6f4746089686c76803225a833d3c0fc459ccc52b
ysdy44 <ysdy44@outlook.com>
2021-11-29 00:48:35 +0800

    Add Video Clip2.Effects

---
+++ Video Clip2/Video Clip2.Effects/Effect.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+
+namespace Video_Clip2.Effects
+{
+    public class Effect
+    {
+        public bool GaussianBlur_IsOn;
+        public float GaussianBlur_Radius = 0;
+        public EffectBorderMode GaussianBlur_BorderMode = EffectBorderMode.Soft;
+
+        public static ICanvasImage Render(Effect effect, ICanvasImage image, float scale)
+        {
+            // GaussianBlur
+            if (effect.GaussianBlur_IsOn)
+            {
+                image = new GaussianBlurEffect
+                {
+                    Source = image,
+                    BlurAmount = effect.GaussianBlur_Radius * scale,
+                    BorderMode = effect.GaussianBlur_BorderMode,
+                };
+            }
+
+            return image;
+        }
+    }
+}
