199974622a007946632dc3274a7d427cf7aca106
ysdy44 <ysdy44@outlook.com>
2021-11-30 00:05:04 +0800

    Use a PreviewCanvas

--- Video Clip2/$DrawPages/DrawPage.Preview.cs
+++ Video Clip2/$DrawPages/DrawPage.Preview.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.Graphics.Canvas;
-using System;
 using Video_Clip2.Clips.ClipManagers;
 using Video_Clip2.Clips.Clips;
 using Video_Clip2.Effects;
-using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -11,8 +9,6 @@ namespace Video_Clip2
 {
     public sealed partial class DrawPage : Page
     {
-
-        double StartingSpliterHeight;
 
         private void ConstructPreview()
         {
@@ -30,7 +26,7 @@ namespace Video_Clip2
                         {
                             if (item.Index == i)
                             {
-                                ICanvasImage image = item.GetRender(this.ViewModel.IsPlayingCore, this.ViewModel.Position, this.PreviewCanvas, this.ViewModel.PreviewSize);
+                                ICanvasImage image = item.GetRender(this.ViewModel.IsPlayingCore, this.ViewModel.Position, this.PreviewCanvas, s.Size);
 
                                 if (image != null)
                                 {
@@ -40,48 +36,6 @@ namespace Video_Clip2
                         }
                     }
                 }
-            };
-
-            this.PreviewGrid.SizeChanged += (s, e) =>
-            {
-                if (e.NewSize == Size.Empty) return;
-                if (e.NewSize == e.PreviousSize) return;
-
-                this.ViewModel.Scale = Math.Min(e.NewSize.Width / this.ViewModel.Size.Width, e.NewSize.Height / this.ViewModel.Size.Height);
-            };
-
-            this.GridSpliterButton.ManipulationStarted += (s, e) =>
-            {
-                double height = this.PreviewRowDefinition.ActualHeight;
-                this.StartingSpliterHeight = height;
-
-                this.PreviewGrid.Width = this.PreviewGrid.ActualWidth;
-                this.PreviewGrid.Height = this.PreviewGrid.ActualHeight;
-                this.PreviewGrid.HorizontalAlignment = HorizontalAlignment.Center;
-                this.PreviewGrid.VerticalAlignment = VerticalAlignment.Top;
-            };
-            this.GridSpliterButton.ManipulationDelta += (s, e) =>
-            {
-                double spliterHeight = this.StartingSpliterHeight + e.Cumulative.Translation.Y;
-                this.PreviewRowDefinition.Height = new GridLength(spliterHeight < 0 ? 0 : spliterHeight);
-
-                double height = this.PreviewRowDefinition.ActualHeight;
-                double scale = height / this.StartingSpliterHeight;
-                this.PreviewScaleTransform.ScaleX = scale;
-                this.PreviewScaleTransform.ScaleY = scale;
-            };
-            this.GridSpliterButton.ManipulationCompleted += (s, e) =>
-            {
-                double spliterHeight = this.StartingSpliterHeight + e.Cumulative.Translation.Y;
-                this.PreviewRowDefinition.Height = new GridLength(spliterHeight);
-
-                this.PreviewScaleTransform.ScaleX = 1;
-                this.PreviewScaleTransform.ScaleY = 1;
-
-                this.PreviewGrid.Width = double.NaN;
-                this.PreviewGrid.Height = double.NaN;
-                this.PreviewGrid.HorizontalAlignment = HorizontalAlignment.Stretch;
-                this.PreviewGrid.VerticalAlignment = VerticalAlignment.Stretch;
             };
         }
 
--- Video Clip2/Video Clip2.ViewModels/ViewModels/ViewModel.Transformer.cs
+++ Video Clip2/Video Clip2.ViewModels/ViewModels/ViewModel.Transformer.cs
@@ -1,26 +1,10 @@
 ﻿using System.ComponentModel;
-using Video_Clip2.Clips.Clips;
-using Windows.Foundation;
 using Windows.Graphics.Imaging;
 
 namespace Video_Clip2.ViewModels
 {
     public partial class ViewModel : INotifyPropertyChanged
     {
-
-
-        public Size PreviewSize
-        {
-            get => this.previewSize;
-            set
-            {
-                this.previewSize = value;
-                this.OnPropertyChanged(nameof(PreviewSize)); // Notify 
-
-                this.Invalidate(); // Invalidate
-            }
-        }
-        public Size previewSize = new Size(1920, 1080);
 
 
         public BitmapSize Size
@@ -30,9 +14,6 @@ namespace Video_Clip2.ViewModels
             {
                 this.size = value;
                 this.OnPropertyChanged(nameof(Size)); // Notify 
-
-                this.PreviewSize = new Size(this.Scale * value.Width, this.Scale * value.Height);
-                this.OnPropertyChanged(nameof(Scale)); // Notify 
             }
         }
         private BitmapSize size = new BitmapSize
@@ -48,9 +29,6 @@ namespace Video_Clip2.ViewModels
             set
             {
                 this.scale = value;
-                this.OnPropertyChanged(nameof(Scale)); // Notify 
-
-                this.PreviewSize = new Size(value * this.Size.Width, value * this.Size.Height);
                 this.OnPropertyChanged(nameof(Scale)); // Notify 
             }
         }
