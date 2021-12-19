84161a4415205146fa4d977e9a7b08d09775bdde
ysdy44 <ysdy44@outlook.com>
2021-12-19 18:15:22 +0800

    Add Meida and Medium

---
+++ Video Clip2/Video Clip2.Medias/Media.cs
@@ -0,0 +1,22 @@
+﻿namespace Video_Clip2.Medias
+{
+    public abstract class Media
+    {
+
+        //@Property
+        public string Name { get; protected set; }
+        public string FileType { get; protected set; }
+        public string Token { get; protected set; }
+
+        public Medium ToMedium()
+        {
+            return new Medium
+            {
+                Name = this.Name,
+                FileType = this.FileType,
+                Token = this.Token,
+            };
+        }
+
+    }
+}
