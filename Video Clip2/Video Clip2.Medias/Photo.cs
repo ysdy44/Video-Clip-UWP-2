95c36ddee9a87d7077666f9e9c6836d837558317
ysdy44 <ysdy44@outlook.com>
2021-12-19 17:29:01 +0800

    Add Photo and Photocopier

---
+++ Video Clip2/Video Clip2.Medias/Photo.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Graphics.Canvas;
+using Windows.Storage;
+
+namespace Video_Clip2.Medias
+{
+    public sealed partial class Photo
+    {
+
+        //@Property
+        public string Name { get; private set; }
+        public string FileType { get; private set; }
+        public string Token { get; private set; }
+
+        public uint Width { get; private set; }
+        public uint Height { get; private set; }
+        public IStorageFile File { get; private set; }
+        public CanvasBitmap Bitmap { get; private set; }
+        public CanvasBitmap Thumbnail { get; private set; }
+
+        public Photocopier ToPhotocopier()
+        {
+            return new Photocopier
+            {
+                Name = this.Name,
+                FileType = this.FileType,
+                Token = this.Token,
+            };
+        }
+
+    }
+}
