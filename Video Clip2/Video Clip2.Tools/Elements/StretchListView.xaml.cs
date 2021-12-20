fdaf4a42de9ec77cdf934789315998acbddd5130
ysdy44 <ysdy44@outlook.com>
2021-12-21 01:48:49 +0800

    StretchListView add IsOverlayLayer

--- Video Clip2/Video Clip2.Tools/Elements/StretchListView.xaml.cs
+++ Video Clip2/Video Clip2.Tools/Elements/StretchListView.xaml.cs
@@ -19,6 +19,12 @@ namespace Video_Clip2.Tools.Elements
         //@Delegate
         /// <summary> Occurs when stretch change. </summary>
         public event EventHandler<Stretch> StretchChanged;
+        /// <summary> Occurs when overlay layer change. </summary>
+        public event EventHandler<bool> IsOverlayLayerChanged;
+
+        //@Converter
+        private Visibility BooleanToVisibilityConverter(bool value) => value ? Visibility.Visible : Visibility.Collapsed;
+        private Visibility ReverseBooleanToVisibilityConverter(bool value) => value ? Visibility.Collapsed : Visibility.Visible;
 
         //@Group
         private readonly IDictionary<Stretch, StretchListViewItem> ItemDictionary = new Dictionary<Stretch, StretchListViewItem>();
@@ -49,8 +55,17 @@ namespace Video_Clip2.Tools.Elements
                     control.ListView.SelectedIndex = -1;
                 }
             }
+        }));
+
+
+        /// <summary> Gets or set the overlay layer state for <see cref="StretchListView"/>. </summary>
+        public bool IsOverlayLayer
+        {
+            get => (bool)base.GetValue(IsOverlayLayerProperty);
+            set => SetValue(IsOverlayLayerProperty, value);
         }
-    ));
+        /// <summary> Identifies the <see cref = "StretchListView.IsOverlayLayer" /> dependency property. </summary>
+        public static readonly DependencyProperty IsOverlayLayerProperty = DependencyProperty.Register(nameof(IsOverlayLayer), typeof(bool), typeof(StretchListView), new PropertyMetadata(false));
 
 
         #endregion
@@ -59,6 +74,16 @@ namespace Video_Clip2.Tools.Elements
         {
             this.InitializeComponent();
             this.InitializeDictionary();
+            this.OverlayLayerButton.Click += (s, e) =>
+            {
+                this.IsOverlayLayer = true;
+                this.IsOverlayLayerChanged?.Invoke(this, true); // Delegate
+            };
+            this.ClipButton.Click += (s, e) =>
+            {
+                this.IsOverlayLayer = false;
+                this.IsOverlayLayerChanged?.Invoke(this, false); // Delegate
+            };
             this.ListView.ItemClick += (s, e) =>
             {
                 if (e.ClickedItem is ContentControl control)
